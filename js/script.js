3d478c1307888e83bde25537b35575a4c2f40a17
Mariusz Borowiak <borowiakm85@gmail.com>
2018-06-05 03:18:18 +0100

    Add generators functions

---
+++ js/script.js
@@ -0,0 +1,26 @@
+(function(){
+  // Random id generator
+  function randomString() {
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    var str = '';
+    for (var i = 0; i < 10; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+  }
+  // Template generator
+  function generateTemplate(name, data, basicElement) {
+    var template = document.getElementById(name).innerHTML;
+    var element = document.createElement(basicElement || 'div');
+
+    Mustache.parse(template);
+    element.innerHTML = Mustache.render(template, data);
+
+    return element;
+  }
+  
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // here we will put the code of our application
+});
+}());
